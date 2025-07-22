19072b531ebf542c1681ae8183538048d7ed67f3
avi <a0548589558@gmail.com>
2025-07-22 12:40:06 +0300

    fixed bug to run faster

--- client/manegers/maneger.py
+++ client/manegers/maneger.py
@@ -4,7 +4,7 @@ import requests
 class Manager:
     def __init__(self):
         # self.URL = "http://bayesian_server:8000/"
-        self.URL = "http://localhost:8000/"
+        self.URL = "http://127.0.0.1:8000/"
 
     def run(self):
         running = True
